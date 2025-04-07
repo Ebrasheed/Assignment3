2d0eba17847c24d40ecc2da88bdbd552b81a176c
ebras <ebras@HooverVacuum>
2025-04-06 23:12:57 -0500

    Added ElectricBassGuitarStrategy class

--- Assignment3/src/mylittlemozart/strategy/ElectricBassGuitarStrategy.java
+++ Assignment3/src/mylittlemozart/strategy/ElectricBassGuitarStrategy.java
@@ -1,5 +1,14 @@
 package mylittlemozart.strategy;
 
-public class ElectricBassGuitarStrategy {
+import mylittlemozart.MidiEventData;
+import mylittlemozart.strategy.InstrumentStrategy;
 
+/**
+ * Sets the instrument to Electric Bass (value 33).
+ */
+public class ElectricBassGuitarStrategy implements InstrumentStrategy {
+    public MidiEventData applyInstrument(MidiEventData original) {
+        original.setInstrument(33);
+        return original;
+    }
 }
