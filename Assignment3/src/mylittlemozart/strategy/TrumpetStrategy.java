839488cf1d75c044f1c76f621622132b4edc253f
ebras <ebras@HooverVacuum>
2025-04-06 23:39:20 -0500

    Added TrumpetStrategy to apply trumpet instrument

--- Assignment3/src/mylittlemozart/strategy/TrumpetStrategy.java
+++ Assignment3/src/mylittlemozart/strategy/TrumpetStrategy.java
@@ -1,5 +1,14 @@
 package mylittlemozart.strategy;
 
-public class TrumpetStrategy {
+import mylittlemozart.MidiEventData;
+import mylittlemozart.strategy.InstrumentStrategy;
 
+/**
+ * Sets the instrument to Trumpet (value 56).
+ */
+public class TrumpetStrategy implements InstrumentStrategy {
+    public MidiEventData applyInstrument(MidiEventData original) {
+        original.setInstrument(56);
+        return original;
+    }
 }
