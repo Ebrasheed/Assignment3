f7302bf13629a3da06326dc23c707ba59b7c56b6
ebras <ebras@HooverVacuum>
2025-04-06 23:35:08 -0500

    added StaccatoMidiEventFactory for generating shorter note-off events

--- Assignment3/src/mylittlemozart/factories/StaccatoMidiEventFactory.java
+++ Assignment3/src/mylittlemozart/factories/StaccatoMidiEventFactory.java
@@ -1,5 +1,21 @@
 package mylittlemozart.factories;
 
-public class StaccatoMidiEventFactory {
+import mylittlemozart.MidiEventData;
 
+/**
+ * Factory for creating staccato MIDI events (shorter duration).
+ */
+public class StaccatoMidiEventFactory extends MidiEventFactoryAbstract {
+    @Override
+    public MidiEventData createNoteOffEvent(MidiEventData baseEvent) {
+        MidiEventData event = new MidiEventData(
+            baseEvent.getStartEndTick() + 5, // Short duration
+            0,
+            baseEvent.getNote(),
+            baseEvent.getChannel(),
+            baseEvent.getInstrument(),
+            0 // Note Off
+        );
+        return event;
+    }
 }
