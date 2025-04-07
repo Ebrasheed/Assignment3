e7597f868f31275da17fc774da1f2ed27ec31c91
ebras <ebras@HooverVacuum>
2025-04-06 23:18:05 -0500

    added LegatoMidiEventFactory for longer notes

--- Assignment3/src/mylittlemozart/factories/LegatoMidiEventFactory.java
+++ Assignment3/src/mylittlemozart/factories/LegatoMidiEventFactory.java
@@ -1,5 +1,21 @@
 package mylittlemozart.factories;
 
-public class LegatoMidiEventFactory {
+import mylittlemozart.MidiEventData;
 
+/**
+ * Factory for creating legato MIDI events (longer duration).
+ */
+public class LegatoMidiEventFactory extends MidiEventFactoryAbstract {
+    @Override
+    public MidiEventData createNoteOffEvent(MidiEventData baseEvent) {
+        MidiEventData event = new MidiEventData(
+            baseEvent.getStartEndTick() + 30,
+            0,
+            baseEvent.getNote(),
+            baseEvent.getChannel(),
+            baseEvent.getInstrument(),
+            0 // Note Off
+        );
+        return event;
+    }
 }
