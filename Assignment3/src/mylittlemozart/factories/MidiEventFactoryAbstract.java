a189628a98d726ffd170c9339fee4d29ae2c9c3f
ebras <ebras@HooverVacuum>
2025-04-06 23:26:10 -0500

    Added MidiEventFactoryAbstract to implement the MidiEventFactory  interface

--- Assignment3/src/mylittlemozart/factories/MidiEventFactoryAbstract.java
+++ Assignment3/src/mylittlemozart/factories/MidiEventFactoryAbstract.java
@@ -1,5 +1,30 @@
 package mylittlemozart.factories;
 
-public class MidiEventFactoryAbstract {
+import mylittlemozart.MidiEventData;
 
+public abstract class MidiEventFactoryAbstract implements MidiEventFactory {
+    public MidiEventData createNoteOnEvent(MidiEventData baseEvent) {
+        MidiEventData event = new MidiEventData(
+                baseEvent.getStartEndTick(),
+                baseEvent.getVelocity(),
+                baseEvent.getNote(),
+                baseEvent.getChannel(),
+                baseEvent.getInstrument(),
+                1 // Note On
+        );
+        return event;
+    }
+
+    public MidiEventData createNoteOffEvent(MidiEventData baseEvent) {
+        MidiEventData event = new MidiEventData(
+                baseEvent.getStartEndTick() + 10,
+                0,
+                baseEvent.getNote(),
+                baseEvent.getChannel(),
+                baseEvent.getInstrument(),
+                0 // Note Off
+        );
+        return event;
+    }
 }
+
