23f3e4f30dc799f045a726ebf060a3fa5bbab288
ebras <ebras@HooverVacuum>
2025-04-06 23:37:28 -0500

    Added StandardMidiEventFactory for default MIDI event creation

--- Assignment3/src/mylittlemozart/factories/StandardMidiEventFactory.java
+++ Assignment3/src/mylittlemozart/factories/StandardMidiEventFactory.java
@@ -1,5 +1,18 @@
 package mylittlemozart.factories;
 
-public class StandardMidiEventFactory {
+import mylittlemozart.MidiEventData;
 
+/**
+ * Factory for creating standard MIDI events.
+ */
+public class StandardMidiEventFactory extends MidiEventFactoryAbstract {
+    @Override
+    public MidiEventData createNoteOnEvent(MidiEventData baseEvent) {
+        return super.createNoteOnEvent(baseEvent);
+    }
+
+    @Override
+    public MidiEventData createNoteOffEvent(MidiEventData baseEvent) {
+        return super.createNoteOffEvent(baseEvent);
+    }
 }
