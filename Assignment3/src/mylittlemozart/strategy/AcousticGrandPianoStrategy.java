89678d3952cf800392dbab4e6750b0e9d5f7355c
ebras <ebras@HooverVacuum>
2025-04-06 23:11:32 -0500

    Added ElectricBassGuitarStrategy class

--- Assignment3/src/mylittlemozart/strategy/AcousticGrandPianoStrategy.java
+++ Assignment3/src/mylittlemozart/strategy/AcousticGrandPianoStrategy.java
@@ -3,9 +3,6 @@ package mylittlemozart.strategy;
 import mylittlemozart.MidiEventData;
 import mylittlemozart.strategy.InstrumentStrategy;
 
-/**
- * Sets the instrument to Acoustic Grand Piano (value 0).
- */
 public class AcousticGrandPianoStrategy implements InstrumentStrategy {
     public MidiEventData applyInstrument(MidiEventData original) {
         original.setInstrument(0);
