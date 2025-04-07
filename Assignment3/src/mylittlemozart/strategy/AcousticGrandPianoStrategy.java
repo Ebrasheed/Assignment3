1b7912478bc42f412a99dfd08f842a309fb38755
ebras <ebras@HooverVacuum>
2025-04-06 23:10:00 -0500

    Added AcousticGrandPianoStrategy

--- Assignment3/src/mylittlemozart/strategy/AcousticGrandPianoStrategy.java
+++ Assignment3/src/mylittlemozart/strategy/AcousticGrandPianoStrategy.java
@@ -1,5 +1,14 @@
 package mylittlemozart.strategy;
 
-public class AcousticGrandPianoStrategy {
+import mylittlemozart.MidiEventData;
+import mylittlemozart.strategy.InstrumentStrategy;
 
+/**
+ * Sets the instrument to Acoustic Grand Piano (value 0).
+ */
+public class AcousticGrandPianoStrategy implements InstrumentStrategy {
+    public MidiEventData applyInstrument(MidiEventData original) {
+        original.setInstrument(0);
+        return original;
+    }
 }
