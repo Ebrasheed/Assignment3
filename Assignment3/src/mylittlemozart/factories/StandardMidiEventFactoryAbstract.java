d31cd7bf1ac151e8427cdbde70981fc72086b9d1
ebras <ebras@HooverVacuum>
2025-04-06 23:38:32 -0500

    Added StandardMidiEventFactoryAbstract for regular note on/off event creation

--- Assignment3/src/mylittlemozart/factories/StandardMidiEventFactoryAbstract.java
+++ Assignment3/src/mylittlemozart/factories/StandardMidiEventFactoryAbstract.java
@@ -1,5 +1,35 @@
 package mylittlemozart.factories;
 
-public class StandardMidiEventFactoryAbstract {
+import mylittlemozart.MidiEventData;
 
+
+public class StandardMidiEventFactoryAbstract extends MidiEventFactoryAbstract {
+
+    /**
+     * Creates a standard note on event.
+     */
+    public MidiEventData createNoteOnEvent(MidiEventData original) {
+        return new MidiEventData(
+            original.getStartEndTick(),
+            original.getVelocity(),
+            original.getNote(),
+            original.getChannel(),
+            original.getInstrument(),
+            1 // Note on
+        );
+    }
+
+    /**
+     * Creates a standard note off event with a fixed offset.
+     */
+    public MidiEventData createNoteOffEvent(MidiEventData original) {
+        return new MidiEventData(
+            original.getStartEndTick() + 100,
+            0,
+            original.getNote(),
+            original.getChannel(),
+            original.getInstrument(),
+            0 // Note off
+        );
+    }
 }
