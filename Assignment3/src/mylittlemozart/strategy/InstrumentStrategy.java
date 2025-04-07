e528084e34166937c2e9bce57d069cca4cdcb202
ebras <ebras@HooverVacuum>
2025-04-06 23:15:46 -0500

    Added InstrumentStategy class

--- Assignment3/src/mylittlemozart/strategy/InstrumentStrategy.java
+++ Assignment3/src/mylittlemozart/strategy/InstrumentStrategy.java
@@ -1,5 +1,10 @@
 package mylittlemozart.strategy;
 
-public class InstrumentStrategy {
+import mylittlemozart.MidiEventData;
 
+/**
+ * Strategy interface for applying different instruments to MIDI events.
+ */
+public interface InstrumentStrategy {
+    MidiEventData applyInstrument(MidiEventData original);
 }
