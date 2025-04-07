5218661bf355a06484aac05f9515f9f6c36fde44
ebras <ebras@HooverVacuum>
2025-04-06 23:36:44 -0500

    added StaccatoMidiEventFactoryAbstract with staccato-style note on/off event generation

--- Assignment3/src/mylittlemozart/factories/StaccatoMidiEventFactoryAbstract.java
+++ Assignment3/src/mylittlemozart/factories/StaccatoMidiEventFactoryAbstract.java
@@ -1,5 +1,35 @@
 package mylittlemozart.factories;
 
-public class StaccatoMidiEventFactoryAbstract {
+import mylittlemozart.MidiEventData;
 
+public class StaccatoMidiEventFactoryAbstract extends MidiEventFactoryAbstract {
+
+    /**
+     * Creates a staccato-style note on event.
+     */
+    public MidiEventData createNoteOnEvent(MidiEventData original) {
+        return new MidiEventData(
+            original.getStartEndTick(),
+            original.getVelocity(),
+            original.getNote(),
+            original.getChannel(),
+            original.getInstrument(),
+            1
+        );
+    }
+
+    /**
+     * Creates a staccato-style note off event with short duration.
+     */
+    public MidiEventData createNoteOffEvent(MidiEventData original) {
+        return new MidiEventData(
+            original.getStartEndTick() + 50,
+            0,
+            original.getNote(),
+            original.getChannel(),
+            original.getInstrument(),
+            0
+        );
+    }
 }
+
