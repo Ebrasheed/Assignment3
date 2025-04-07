7f4c0b9379a7d782aa78e0eb9417131818a6b1eb
ebras <ebras@HooverVacuum>
2025-04-06 23:15:00 -0500

    added HigherPitchStrategy class

--- Assignment3/src/mylittlemozart/strategy/HigherPitchStrategy.java
+++ Assignment3/src/mylittlemozart/strategy/HigherPitchStrategy.java
@@ -1,5 +1,14 @@
 package mylittlemozart.strategy;
 
-public class HigherPitchStrategy {
+import mylittlemozart.MidiEventData;
+import mylittlemozart.strategy.PitchStrategy;
 
+/**
+ * Increases pitch by one octave.
+ */
+public class HigherPitchStrategy implements PitchStrategy {
+    public MidiEventData applyPitch(MidiEventData original) {
+        original.setNote(original.getNote() + 12);
+        return original;
+    }
 }
