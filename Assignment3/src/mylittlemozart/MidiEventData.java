67137ebbb4de1a6e27e196df1df6e3dfd188222c
ebras <ebras@HooverVacuum>
2025-04-06 23:33:27 -0500

    Added MidiEventData class to represent parsed MIDI events from CSV

--- Assignment3/src/mylittlemozart/MidiEventData.java
+++ Assignment3/src/mylittlemozart/MidiEventData.java
@@ -1,5 +1,55 @@
 package mylittlemozart;
 
+/**
+ * Represents a MIDI event extracted from CSV.
+ */
 public class MidiEventData {
+    private int startEndTick;
+    private int velocity;
+    private int note;
+    private int channel;
+    private int instrument;
+    private int noteOnOff;
 
+    public MidiEventData(int startEndTick, int velocity, int note, int channel, int instrument, int noteOnOff) {
+        this.startEndTick = startEndTick;
+        this.velocity = velocity;
+        this.note = note;
+        this.channel = channel;
+        this.instrument = instrument;
+        this.noteOnOff = noteOnOff;
+    }
+
+    public int getStartEndTick() {
+        return startEndTick;
+    }
+
+    public int getVelocity() {
+        return velocity;
+    }
+
+    public int getNote() {
+        return note;
+    }
+
+    public int getChannel() {
+        return channel;
+    }
+
+    public int getInstrument() {
+        return instrument;
+    }
+
+    public int getNoteOnOff() {
+        return noteOnOff;
+    }
+    
+    public void setNote(int note) {
+    	this.note = note; 
+    }
+
+    public void setInstrument(int instrument) {
+        this.instrument = instrument;
+    }
 }
+    
